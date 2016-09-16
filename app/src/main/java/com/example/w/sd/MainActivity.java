18c6318a8da030bf0fa2ffbb9d1ac8c0c1d10584
w <1379853502@qq.com>
2016-09-16 12:32:27 +0800

    Initial commit

---
+++ app/src/main/java/com/example/w/sd/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.w.sd;
+
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+public class MainActivity extends AppCompatActivity {
+
+    //文件名
+    private final static String MyFileName="myfile";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        Button btnWrite=(Button)findViewById(R.id.ButtonWrite);
+        Button btnRead=(Button)findViewById(R.id.ButtonRead);
+        btnWrite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OutputStream out=null;
+                try {
+                    if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+                        File file = Environment.getExternalStorageDirectory();
+                        File myfile = new File(file.getCanonicalPath() + "/"+MyFileName);
+
+                        FileOutputStream fileOutputStream = new FileOutputStream(myfile);
+                        out = new BufferedOutputStream(fileOutputStream);
+                        EditText editText1 = (EditText) findViewById(R.id.name);
+                        String content = editText1.getText().toString();
+                        try {
+                            out.write(content.getBytes(StandardCharsets.UTF_8));
+                        } finally {
+                            if (out != null)
+                                out.close();
+                        }
+                    }
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        btnRead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                InputStream in=null;
+                try {
+
+                    if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+                        File file = Environment.getExternalStorageDirectory();
+                        File myfile = new File(file.getCanonicalPath() + "/"+ MyFileName);
+                        FileInputStream fileInputStream = new FileInputStream(myfile);
+                        in = new BufferedInputStream(fileInputStream);
+
+                        int c;
+                        StringBuilder stringBuilder = new StringBuilder("");
+                        try {
+                            while ((c = in.read()) != -1) {
+                                stringBuilder.append((char) c);
+                            }
+                            Toast.makeText(MainActivity.this, stringBuilder.toString(), Toast.LENGTH_LONG).show();
+                        } finally {
+                            if (in != null)
+                                in.close();
+                        }
+                    }
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+
+
+            }
+        });
+    }
+}
